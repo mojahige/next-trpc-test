4da71d1781e496664b60b763076e3fc844493b21
mojahige <mojahige@gmail.com>
2022-08-18 12:49:41 +0900

    Tried "Use with Next.js"
    https://trpc.io/docs/nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { withTRPC } from "@trpc/next";
+import { AppRouter } from "@/pages/api/trpc/[trpc]";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ ctx: _ctx }) {
+    const url = "http://localhost:3000/api/trpc";
+
+    return {
+      url,
+    };
+  },
+})(MyApp);
